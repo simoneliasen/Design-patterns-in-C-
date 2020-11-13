4c024823000bdb806b1d0053dd2ed56f991d684c
Simon <simoneliasen@gmail.com>
2020-11-13 18:45:32 +0100

    small additions to singleton code + comments to be implemented as code, missing thread safe version + examples

--- Creational/Singleton.cs
+++ Creational/Singleton.cs
@@ -1,5 +1,12 @@
+
+// Simple Singleton implementation - Not-thread safe.
 class Singleton {
-    private static Singleton instance; // SHould instance be underlined eg. _instance? should this be set as readonly?
+    private static Singleton instance; // Should instance be underlined eg. _instance? should this be set as readonly?
+
+    // Is it necessary to declare an empty instance property?
+    private PatternA()
+    {
+    }
 
     public static Singleton GetInstance()
     {
@@ -10,3 +17,24 @@ class Singleton {
     }
 
 }
+
+/*
+Description:
+- Global acess to class with only one instance.
+
+
+Singleton Applications:
+- Database connection
+- API Token
+
+Pros:
+- Ensure that there is only one instance.
+- Acess it globally.
+
+Cons:
+- Goes against the Single-resposibility principle. 
+- Can hide elements, making it harder to test.
+- Multithreading can possibly break the pattern.
+
+
+*/
